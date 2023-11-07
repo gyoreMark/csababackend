0d242ad7d29875dfe8413d14430832032689100d
Györe Márk <gyore.mark@students.jedlik.eu>
2023-11-07 22:33:43 +0100

    n

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-import axios from "axios"; const dataProvider = () => { return{ getAdjnekinevet:
-async () => { let url = https://api.projektnet.ddc.sze.hu/api/nevek/; try { let
-response = await axios.get(url); return response.data; } catch (error) {
-console.log(error); return []; } }, } };
+import axios from "axios";
+const dataProvider = () => { 
+    return{ 
+        getAdjnekinevet:async () => { 
+            let url = https://api.projektnet.ddc.sze.hu/api/nevek/; 
+            try { 
+                let response = await axios.get(url); 
+                return response.data; } 
+                catch (error) {
+                    console.log(error); 
+                    return []; 
+                } 
+            }, 
+        } 
+    };
